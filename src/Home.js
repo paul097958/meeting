8b5ade9276f6235ce5f00beaa9e8eb842ef2fc78
Paul <paul097958@gmail.com>
2022-10-21 01:09:12 +0800

    fix css

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { Input, Button, IconButton } from '@material-ui/core';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import "./Home.css"
+import { Input, Button, Typography } from '@material-ui/core';
+import ShortUniqueId from 'short-unique-id';
+
+const uid = new ShortUniqueId({ length: 10 });
 
 class Home extends Component {
-  	constructor (props) {
+	constructor(props) {
 		super(props)
 		this.state = {
-			url: ''
+			url: uid()
 		}
 	}
 
@@ -16,7 +17,7 @@ class Home extends Component {
 	join = () => {
 		if (this.state.url !== "") {
 			var url = this.state.url.split("/")
-			window.location.href = `/${url[url.length-1]}`
+			window.location.href = `/${url[url.length - 1]}`
 		} else {
 			var url = Math.random().toString(36).substring(2, 7)
 			window.location.href = `/${url}`
@@ -25,26 +26,30 @@ class Home extends Component {
 
 	render() {
 		return (
-			<div className="container2">
-				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
-					Source code: 
-					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>
-						<GitHubIcon />
-					</IconButton>
-				</div>
-				
+			<div style={{
+				height: '100vh',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center'
+			}}>
 				<div>
-					<h1 style={{ fontSize: "45px" }}>Video Meeting</h1>
-					<p style={{ fontWeight: "200" }}>Video conference website that lets you stay in touch with all your friends.</p>
-				</div>
-
-				<div style={{
-					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
-					textAlign: "center", margin: "auto", marginTop: "100px"
-				}}>
+					<Typography
+						variant='h3'
+						component="h1"
+						margin="5"
+					>
+						Classroomeditor
+					</Typography>
 					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
-					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
-					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
+					<Input
+						placeholder="URL"
+						style={{
+							fontSize: '35px',
+							margin: '20px'
+						}}
+						onChange={e => this.handleChange(e)}
+						value={this.state.url} />
+					<Button variant="contained" color="primary" onClick={this.join}>Go</Button>
 				</div>
 			</div>
 		)
